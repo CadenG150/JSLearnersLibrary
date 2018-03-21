685a23d8381cd1dfc4337701721f65471029d2fb
Caden Grey <30481164+CadenG150@users.noreply.github.com>
2018-03-21 14:53:46 -0400

    Add files via upload

---
+++ example/documentation.js
@@ -0,0 +1,31 @@
+documentation = "This is a JavaScript library that helps people code easier.  \
+It contains many functions that have snippets to make JavaScript coding easier to \
+remember and use.  In order for this library to work properly you have to link the \
+.js files at the bottom of your body."
+
+logdocumentation = "The \"Log\" function is a function that makes using console.\
+log much easier.  All you have to do is put this into your main.js file: log(\"Hello Worl\
+d\").  This will log \"Hello World\" to the console.";
+
+writedocumentation = "The \"Write\" function is a function that makes using document.\
+write much easier.  All you have to do is put this into your main.js file: write(\"Hello W\
+orld\").  This will write \"Hello World\" to the screen."
+
+createbuttondocumentation = "The \"CreateButton\" function is a function that makes \
+creating a button much simpler.  Al you have to do is put this into your main.js file: \
+createbutton(\"Test\", \"id1\", \"alert\", \"Hello\")  The first parameter is the \
+text that the button displays.  The second parameter is the buttons id and class.  \
+The third parameter is what will happen when the button is clicked.  Supported types \
+include: alert, console, and write.  The fourth parameter is what text will be displ\
+ayed after the button is clicked."
+
+styledocumentation = " The \"Style\" function is a function that makes styling an \
+element much simpler.  All you have to do is put this into your main.js file: \
+style(\"id1\", \"color\", \"blue\")  The first parameter is the id of the element \
+that you want to change.  The second parameter is what you want to change about \
+the element.  The third parameter is the modifier for parameter 2.  So in the \
+above example, you would be changing the color of the id1 element to blue. "
+
+docdocumentation = "The \"Doc\" function is a function that gives you information \
+about a specific function.  All you have to do is put this into your main.js \
+file: doc(\"style\")  This will display information about the style function."
---
+++ example/main.js
@@ -0,0 +1,6 @@
+log("Hello World");
+write("Hi");
+createbutton("Test", "id1", "alert", "Hello");
+style("test", "color", "blue");
+style("test", "textalign", "center");
+doc("style");
